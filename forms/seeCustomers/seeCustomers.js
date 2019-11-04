d79c498953888458e25c2eb5d70db03787e52fa3
Priyanka Gupta <pgu04554@creighton.edu>
2019-11-03 22:59:28 -0600

    assignment

--- forms/seeCustomers/seeCustomers.js
+++ forms/seeCustomers/seeCustomers.js
@@ -1,8 +1,7 @@
 
 seeCustomers.onshow=function(){
    let query = "SELECT name FROM customer;" 
-  req1 = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=scs63109&pass=3hGm9xnt&database=scs63109&query=" + query)
-
+  req1 = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=pgu04554&pass=Priyanka98*&database=Priyanka98*&query=" + query)
     if (req1.status == 200) { 
         let results = JSON.parse(req1.responseText)
               
@@ -19,3 +18,35 @@ seeCustomers.onshow=function(){
 }
 
 
+
+
+
+
+drpCompanies.onclick=function(){
+if (typeof(s)=="object"){
+  return
+  else{
+  customer = s
+
+      let query1 = "SELECT name,street,city,state,zipcode FROM customer WHERE name=" + '"'+customer+'"'
+      req1 = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=pgu04554&pass=Priyanka98*&database=pgu04554&query=" + query1)
+    
+        if (req1.status == 200) { 
+
+      let results = JSON.parse(req2.responseText)
+          console.log(results)
+           if (results.length == 0){
+        NSB.MsgBox("There are no customers of that name.")
+   } else {        
+        console.log("the parsed JSON is " + results)
+
+        results = results[0]
+        message = (`${results[1]} \n${results[2]} \n${results[3]}, ${results[4]}, ${results[5]}`);
+        console.log(message)
+        txtResult.value = message;
+}
+    } else
+ }      
+
+        NSB.MsgBox("Error code: " + req2.status)
+}
